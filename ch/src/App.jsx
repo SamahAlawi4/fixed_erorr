26dfdf7cd0dd3058f8da674f116f3ad5bddfe723
SamahAllawi2 <samah.alawi4@icloud.com>
2024-09-06 18:06:30 +0300

    fixedErorr

---
+++ ch/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar.jsx'
+import Contact from './components/Contact.jsx'
+import Player from './components/Player.jsx'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+
+export default function App() {
+  return (
+   <>
+   <Navbar />
+   <Contact />
+   <Player />
+   </>
+  )
+}
---
+++ ch/src/components/Player.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+export default function Player() {
+  const [player, setPlayer] = useState({
+    firstName: 'Ranjani',
+    lastName: 'Shettar',
+    score:0,
+  });
+
+  const [score,setCounter]=useState(0);
+  const increment =()=>{
+    setCounter(score +1);
+    
+  }
+
+
+ 
+
+  function handleFirstNameChange(e) {
+    setPlayer({
+      ...player,
+      firstName: e.target.value,
+    });
+  }
+
+  function handleLastNameChange(e) {
+    setPlayer({
+      lastName: e.target.value
+    });
+  }
+
+  return (
+    <>
+      <label>
+        Score: <b>{score}</b>
+        {' '}
+        <button onClick={increment}>
+          +1
+        </button>
+
+      </label>
+      <label>
+        First name:
+        <input
+          value={player.firstName}
+          onChange={handleFirstNameChange}
+        />
+      </label>
+      <label>
+        Last name:
+        <input
+          value={player.lastName}
+          onChange={handleLastNameChange}
+        />
+      </label>
+    </>
+  );
+  }
